390640d6f780b6d1d59fe5eeff0b797b0bcfe04a
Jan Kück <jkueck@cargosoft.de>
2021-04-14 08:17:31 +0200

    remove comments text

--- src/main/java/de/jkueck/fire/SystemSettings.java
+++ src/main/java/de/jkueck/fire/SystemSettings.java
@@ -22,33 +22,4 @@ public enum SystemSettings {
         this.value = value;
     }
 
-    /* public static final String SYSTEM_SETTING_APPLICATION_NAME = "applicationName";
-    public static final String SYSTEM_SETTING_TELEGRAM_BOT_ID = "telegramBotId";
-    public static final String SYSTEM_SETTING_IS_TELEGRAM_ENABLED = "isTelegramEnabled";
-    public static final String SYSTEM_SETTING_DIVERA_ACCESS_KEY = "diveraAccessKey";
-    public static final String SYSTEM_SETTING_IS_DIVERA_ENABLED = "isDiveraEnabled";
-    public static final String SYSTEM_SETTING_DIVERA_MESSAGE = "diveraMessage";
-    public static final String SYSTEM_SETTING_ALERT_MESSAGE_DATE_TIME_FORMAT = "alertMessageDateTimeFormat";
-    public static final String SYSTEM_SETTING_IS_DATABASE_PERSIST_ENABLED = "isDatabasePersistEnabled";
-    public static final String SYSTEM_SETTING_COM_PORT = "comPort";
-
-    @Getter
-    private final LinkedHashMap<String, SystemSetting> systemSettingMap = new LinkedHashMap<>();
-
-    public SystemSettings() {
-        this.addName(SYSTEM_SETTING_APPLICATION_NAME, "fire");
-        this.addName(SYSTEM_SETTING_TELEGRAM_BOT_ID, "1234-telegram");
-        this.addName(SYSTEM_SETTING_DIVERA_ACCESS_KEY, "1234-divera");
-        this.addName(SYSTEM_SETTING_IS_TELEGRAM_ENABLED, "true");
-        this.addName(SYSTEM_SETTING_IS_DIVERA_ENABLED, "true");
-        this.addName(SYSTEM_SETTING_DIVERA_MESSAGE, "lorem ipsum");
-        this.addName(SYSTEM_SETTING_ALERT_MESSAGE_DATE_TIME_FORMAT, "dd.MM.yyyy HH:mm");
-        this.addName(SYSTEM_SETTING_IS_DATABASE_PERSIST_ENABLED, "true");
-        this.addName(SYSTEM_SETTING_COM_PORT, "xxx");
-    }
-
-    private void addName(String name, String value) {
-        this.systemSettingMap.put(name, new SystemSetting(name, value));
-    } */
-
 }
